7c7f9cc44bd3cfc987f4ef469dc46b399466382c
Gary <gn02519960@gmail.com>
2016-08-23 11:16:34 +0000

    make it better

--- models/favourite.js
+++ models/favourite.js
@@ -1,16 +1,4 @@
-/*
-var mongoose = require("mongoose");
-
-mongoose.connect('mongodb://collection:45830027@ds023664.mlab.com:23664/sampledb305cde2016', function(err) {
-     if(err) {
-         console.log('connection error', err);
-     } else {
-         console.log('connection successful');
-     }
- });
- */
- 
- var mongoose = require('../models/db.js');
+var mongoose = require('../models/db.js');
 
 var FavouriteSchema = new mongoose.Schema({
    volume_id: String,
--- models/member.js
+++ models/member.js
@@ -1,14 +1,3 @@
-/*
-var mongoose = require("mongoose");
-
-mongoose.connect('mongodb://collection:45830027@ds023664.mlab.com:23664/sampledb305cde2016', function(err) {
-     if(err) {
-         console.log('connection error', err);
-     } else {
-         console.log('connection successful');
-     }
- });
-*/
 var mongoose = require('../models/db.js');
 var MemberSchema = new mongoose.Schema({
    user_name: String,
@@ -22,32 +11,5 @@ var MemberSchema = new mongoose.Schema({
 var member = mongoose.model('Member', MemberSchema);
 
 var register;
-/*
-exports.all = function() { 
-    var res_data;
-  Member.find({}, function(err, data){
-        if(err)
-            res_data = err;
-        else
-            res_data = data;
-    })
-  return res_data;
-}
 
-
-exports.register = function(user_name, password, name, email) {
-  member.create({
-       user_name: user_name,
-       password: password,
-       name: name,
-       email: email,
-       status: false,
-  },function(err, todo){
-   if(err)
-      return false;
-   else 
-      return true;
-  });
-}
-*/
 module.exports = member;
